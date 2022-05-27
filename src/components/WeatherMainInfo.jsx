dc4a044942e48ea4dc3f1f7e91c10d6e4ce44139
genesisbpa10 <genesisbetaly@gmail.com>
2022-05-27 13:39:46 -0300

    fifth-commit-errorSolvedPendingToChangeTheZoomImg

--- src/components/WeatherMainInfo.jsx
+++ src/components/WeatherMainInfo.jsx
@@ -22,8 +22,12 @@ function WeatherMainInfo({ weather }) {
       </div>
       <div>
         {/*Falta solucionar el problema con la URL que proporciona Google Maps para obtener el mapa con la locación buscada. El actual es del de Londres, puesto por defecto, pero el objetivo es actualizar el estado (cosa que sí se está logrando, pero no logro armart una URL correcta que me de la ubicación buscada porque no encuentro el formato/orden/significado de los parámetros de la URL para poder armar la nuea desde ${weather?.location.lon} y ${weather?.location.lat}*/}
+
+
         <iframe
-          src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317715.71193692513!2d${weather?.location.lon}2!3d${weather?.location.lon}!2m3!1f0!2f0!3f0!3m2!1i1!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondres%2C%20Reino%20Unido!5e0!3m2!1ses-419!2sar!4v1653599679399!5m2!1ses-419!2sar`}
+          src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d8116210.903910733!2d${weather?.location.lon}!3d${weather?.location.lat}8!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sar!4v1653668549120!5m2!1sfr!2sar`}
+
+          // src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317715.71193692513!2d${weather?.location.lon}!3d${weather?.location.lat}5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondres%2C%20Royaume-Uni!5e0!3m2!1sfr!2sar!4v1653668597580!5m2!1sfr!2sar`}
           width="100%"
           height="450"
           style={{ border: 0 }}
@@ -34,5 +38,5 @@ function WeatherMainInfo({ weather }) {
       </div>
     </div>
   );
-}
+};
 export default WeatherMainInfo;
